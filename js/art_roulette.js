1696c4cb3a8c97913297e9976169f89f569ba2aa
djpelu <116528023+djpelu@users.noreply.github.com>
2025-04-18 00:20:13 +0200

    bug fixing

--- js/art_roulette.js
+++ js/art_roulette.js
@@ -173,7 +173,7 @@ const popupTitle = document.getElementById('AR_popup_title');
 const popupDescr = document.getElementById('AR_popup_description');
 const popupLink = document.getElementById('AR_popup_link');
 const popupVai = document.getElementById('AR_popup_vai');
-const overlay = document.getElementById('AR_popup_overlay');
+const overlay = document.querySelector('.overlay_popup');
 
 function Roulette() {
 
