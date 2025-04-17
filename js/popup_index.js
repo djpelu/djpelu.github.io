b353e7398c06155bc7e4dcd3dbfb7514e0609b51
djpelu <116528023+djpelu@users.noreply.github.com>
2025-04-17 21:12:58 +0200

    ottimizzazione popup index

---
+++ js/popup_index.js
@@ -0,0 +1,39 @@
+const allarmePopup = document.getElementById('allarme')
+const closePopup = document.querySelector('.x')
+const overlayPopup = document.querySelector('.overlay_popup')
+
+// rilevo dispositivo utente
+function isMobileOrTablet() {
+    return /Mobi|Android|Tablet|iPad|iPhone|iPod/i.test(navigator.userAgent);
+}
+
+// mostra pop-up SE su mobile e l'utente NON ha già visto il popup
+if (isMobileOrTablet() && !sessionStorage.getItem('popupShown')) {
+    setTimeout(function() {
+        overlayPopup.style.display = 'block';
+        document.body.style.overflow = 'hidden'; // blocca lo scroll
+        allarmePopup.style.display = 'inherit';
+        // una volta che l'utente ha visto il popup salvo questa informazione
+        sessionStorage.setItem('popupShown', 'true');
+    }, 2000); // 3000 millisecondi = 3 sec per far apparire popup
+}
+
+// chiudi popup quando si clicca X
+closePopup.addEventListener('click', function() {
+    allarmePopup.style.display = 'none';
+    overlayPopup.style.display = 'none';
+    document.body.style.overflow = 'auto';
+});
+
+// chiudi popup se si clicca fuori da esso
+document.addEventListener('click', () => {
+    overlayPopup.style.display = 'none';
+    allarmePopup.style.display = 'none';
+    document.body.style.overflow = 'auto';
+  });
+  
+// previeni chiusura quando si clicca dentro popup
+allarmePopup.addEventListener('click', (e) => {
+e.stopPropagation();
+});
+
