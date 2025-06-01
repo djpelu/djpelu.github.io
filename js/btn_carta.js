3781b4ef3ec2e4c3da76b65b16bc55cb0f770993
djpelu <116528023+djpelu@users.noreply.github.com>
2025-06-02 00:50:41 +0200

    ALTRI! creato in una notte maniacale

---
+++ js/btn_carta.js
@@ -0,0 +1,17 @@
+// scartoccia
+document.getElementById('btn_carta').addEventListener('click', function () {
+  const contenuto = document.getElementById('altri_main');
+  const bottone = document.getElementById('btn_carta');
+
+  contenuto.classList.remove('nascosto');
+  bottone.classList.add('nascosto');
+});
+
+// accartoccia
+document.getElementById('altri_main').addEventListener('click', function () {
+  const contenuto = document.getElementById('altri_main');
+  const bottone = document.getElementById('btn_carta');
+
+  contenuto.classList.add('nascosto');
+  bottone.classList.remove('nascosto');
+});
