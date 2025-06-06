8ff9bf8949015fe9e9db7f52cc5242211ecd89f8
djpelu <116528023+djpelu@users.noreply.github.com>
2025-06-06 15:51:26 +0200

    bug fixing cuore hover

--- js/btn_cuore.js
+++ js/btn_cuore.js
@@ -1,9 +1,11 @@
 
-// precarica mmagine hover
+//gestione hover btn cuore perchè la soluzione con css non è supportata da tutti browser
+
+// precarica mmagine hover (altrimenti quando l'utente clicca, non è ancora caricata e scatta il macello)
 const imgHover = new Image();
 imgHover.src = "images/btn_cuore_hover.png";
 
-//gestisce hover btn cuore perchè la soluzione con css non è supportata da tutti browser
+// gestisci hover
 document.querySelectorAll('.btn_cuore').forEach(button => {
   const img = button.querySelector('.btn_cuore_img');
 
@@ -12,6 +14,14 @@ document.querySelectorAll('.btn_cuore').forEach(button => {
   });
 
   button.addEventListener("mouseout", () => {
+    img.src = "images/btn_cuore.png";
+  });
+});
+
+// evita che quando un utente torna alla home, il bottone sia ancora in hover
+window.addEventListener('pageshow', () => {
+  document.querySelectorAll('.btn_cuore').forEach(button => {
+    const img = button.querySelector('.btn_cuore_img');
     img.src = "images/btn_cuore.png";
   });
 });
